a4b320a856984754dbec0f43b83c9d413be674cd
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-28 20:00:07 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -16,29 +16,21 @@ class EventLogger:
     def __init__(self, data=None):
         if data is None:
             # Initialize with dummy values
-            self.data = [{
+            self.data = {
                 "Event": [
                     {
                         "name": "DefaultEvent",
-                        "data": {
-                            "datetime": str(datetime.now()),
-                            "severity": Severity.INFO.value,
-                            "caller": self.get_caller_function(),
-                            "response": Response.SUCCESS.value,
-                            "message": "Default message"
-                        }
+                        "data": self.get_default_data()
                     }
                 ]
-            }]
+            }
         else:
             # Update with provided data, filling missing values with defaults
             self.data = self.fill_defaults(data)
 
     def fill_defaults(self, data):
-        for event in data:
-            if "Event" in event:
-                for sub_event in event["Event"]:
-                    sub_event["data"] = self.fill_default_values(sub_event.get("data", {}))
+        for event in data.get("Event", []):
+            event["data"] = self.fill_default_values(event.get("data", {}))
         return data
 
     def fill_default_values(self, data):
@@ -51,6 +43,15 @@ class EventLogger:
         }
         data.update({key: data.get(key, value) for key, value in default_values.items()})
         return data
+
+    def get_default_data(self):
+        return {
+            "datetime": str(datetime.now()),
+            "severity": Severity.INFO.value,
+            "caller": self.get_caller_function(),
+            "response": Response.SUCCESS.value,
+            "message": "Default message"
+        }
 
     def get_caller_function(self):
         # Get the name of the calling function using inspect
@@ -67,18 +68,16 @@ event_logger_1 = EventLogger()
 print(event_logger_1.to_json())
 
 # Case 2: Provide data with some fields missing
-custom_data = [
-    {
-        "Event": [
-            {
-                "name": "CustomEvent",
-                "data": {
-                    "severity": Severity.WARNING.value,
-                    "message": "Custom message"
-                }
+custom_data = {
+    "Event": [
+        {
+            "name": "CustomEvent",
+            "data": {
+                "severity": Severity.WARNING.value,
+                "message": "Custom message"
             }
-        ]
-    }
-]
+        }
+    ]
+}
 event_logger_2 = EventLogger(custom_data)
 print(event_logger_2.to_json())
