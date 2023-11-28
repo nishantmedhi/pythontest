3521bdae4a59730fe9bd9b7b2e2de63cd666894a
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-28 12:58:04 +0530

    Update common.py

--- common.py
+++ common.py
@@ -27,10 +27,10 @@ class Logger:
 			                    }
 				        }
 				    ]
-		} ]
+			} ]
         else:
-		self.json_data = []
-		self.json_data.name = json_data["name"]
+			self.json_data = []
+			self.json_data.name = json_data["name"]
         	self.json_data.data = json_data["data"]	
 
     @staticmethod
