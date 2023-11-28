958d19e944340b96a8c2163166a4bdcf8d6eff54
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-28 13:20:16 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -3,26 +3,35 @@ from datetime import datetime
 
 class DataProcessor:
     def __init__(self, events=None):
+        # Default values for event data fields
+        default_values = {
+            "datetime": str(datetime.now()),
+            "severity": "High",
+            "response": "Action taken",
+            "message": "Critical issue"
+        }
+
         if events is None:
             # If no events are provided, use a default event
             self.events = [
                 {
                     "name": "Default",
-                    "data": {
-                        "datetime": str(datetime.now()),
-                        "severity": "High",
-                        "response": "Action taken",
-                        "message": "Critical issue"
-                    }
+                    "data": default_values
                 }
             ]
         else:
-            # If events are provided, use them
+            # If events are provided, use them with default values for missing fields
             self.events = []
             for event_name, event_data in events.items():
+                custom_event_data = []
+                for item in event_data:
+                    # Fill missing fields with default values
+                    filled_data = {**default_values, **json.loads(item)}
+                    custom_event_data.append(filled_data)
+
                 self.events.append({
                     "name": event_name,
-                    "data": [json.loads(item) for item in event_data]
+                    "data": custom_event_data
                 })
 
     def print_events(self):
@@ -36,11 +45,11 @@ class DataProcessor:
 processor1 = DataProcessor()
 processor1.print_events()
 
-# If events are provided, they will be used with dynamic names
+# If events are provided, missing data fields will be filled with default values
 event_data = {
     "CustomEvent1": [
-        '{"datetime": "2023-01-01 12:00:00", "message": "Custom issue"}',
-        '{"datetime": "2023-01-02 15:30:00", "severity": "Low", "response": "No action required", "message": "Another custom message"}'
+        '{"datetime": "2023-01-01 12:00:00", "severity": "Medium"}',  # Missing response and message fields
+        '{"datetime": "2023-01-02 15:30:00"}'  # Missing severity, response, and message fields
     ],
     "CustomEvent2": [
         '{"datetime": "2023-01-03 09:45:00", "severity": "High", "response": "Action taken", "message": "Critical custom issue"}'
