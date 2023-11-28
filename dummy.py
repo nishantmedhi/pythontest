f37f99205e0cbbbfc53eda3188c51c252535cc88
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-28 23:00:34 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -18,17 +18,6 @@ class EventProcessor:
     def __init__(self, events=None):
         if events is None:
             events = []
-                """{
-                    "name": "default_event",
-                    "data": {
-                        "datetime": datetime.now().isoformat(),
-                        "severity": Severity.INFO.value,
-                        "caller": inspect.stack()[1][3],
-                        "response": Response.FAILURE.value,
-                        "message": "Default message from init"
-                    }
-                }"""
-            #]
         self.events = events
 
     def process_data(self):
