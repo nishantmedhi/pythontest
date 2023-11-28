c45d4a7f2299fa1e62656c17c9e8504d0365abd1
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-29 03:13:40 +0530

    Create upgrage_java_install_windows.py

---
+++ upgrage_java_install_windows.py
@@ -0,0 +1,45 @@
+from common import Logger
+from record import EventRecorder
+import os
+
+logger = Logger()
+eventRecorder = EventRecorder()
+update_java_version = os.environ['TO_VERSION']
+
+def verify_java_upgrade_install_windows():
+    eventName = "Verify java upgrade version in Windows"
+    caller = logger.get_caller()
+    try:
+        print(eventName)
+        if "11.0.11" in update_java_version:
+            update_logs(eventRecorder.record(eventName, caller, "SUCCESS"))
+        else:
+            message = "to_version in workflow input is below the prescribed version for update"
+            update_logs(eventRecorder.record(eventName, caller, "FAILURE", "ERROR", message))
+            
+        upgrage_java_install_windows()
+            
+    except Exception as e:
+        update_logs(eventRecorder.record(eventName, caller, "FAILURE", "EXCEPTION", str(e)))
+
+def upgrage_java_install_windows():
+    eventName = "Installing java version " + update_java_version + " in Windows"
+    caller = logger.get_caller()
+    try:
+        print(eventName)
+        if "11.0.11" in eventName:
+            update_logs(eventRecorder.record(eventName, caller, "SUCCESS"))
+        else:
+            message = "Installation of java version " + update_java_version + " failed in Windows"
+            update_logs(eventRecorder.record(eventName, caller, "FAILURE", "ERROR", message))
+            
+    except Exception as e:
+        update_logs(eventRecorder.record(eventName, caller, "FAILURE", "EXCEPTION", str(e)))
+       
+def update_logs(eventData):
+    logger_data = Logger(eventData)
+    logger_data.write_to_file()
+    logger.read_and_print_file()
+    
+if __name__ == "__main__":
+    verify_java_upgrade_install_windows()
