dbdd1d165ba5980ea2a1fb7f46aa97a3bd6b4633
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-11-28 13:16:34 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -4,38 +4,42 @@ from datetime import datetime
 class DataProcessor:
     def __init__(self, events=None):
         if events is None:
-            # If no events are provided, use dummy values for two events
-            self.events = {
-                "Event1": [
-                    {"datetime": str(datetime.now()), "severity": "High", "response": "Action taken", "message": "Critical issue"},
-                    {"datetime": str(datetime.now()), "severity": "Medium", "response": "Investigating", "message": "Potential problem"}
-                ],
-                "Event2": [
-                    {"datetime": str(datetime.now()), "severity": "Low", "response": "No action required", "message": "Informational message"},
-                    {"datetime": str(datetime.now()), "severity": "High", "response": "Action taken", "message": "Another critical issue"}
-                ]
-            }
+            # If no events are provided, use a default event
+            self.events = [
+                {
+                    "name": "Default",
+                    "data": {
+                        "datetime": str(datetime.now()),
+                        "severity": "High",
+                        "response": "Action taken",
+                        "message": "Critical issue"
+                    }
+                }
+            ]
         else:
             # If events are provided, use them
-            self.events = {}
+            self.events = []
             for event_name, event_data in events.items():
-                self.events[event_name] = [json.loads(item) for item in event_data]
+                self.events.append({
+                    "name": event_name,
+                    "data": [json.loads(item) for item in event_data]
+                })
 
     def print_events(self):
-        for event_name, event_data in self.events.items():
-            print(f"\nEvent: {event_name}")
-            for data in event_data:
+        for event in self.events:
+            print(f"\nEvent: {event['name']}")
+            for data in event['data']:
                 print(f"  Datetime: {data['datetime']}, Severity: {data['severity']}, Response: {data['response']}, Message: {data['message']}")
 
 # Example usage:
-# If no events are provided, dummy values will be used for two events
+# If no events are provided, a default event will be used
 processor1 = DataProcessor()
 processor1.print_events()
 
-# If events are provided, they will be used
+# If events are provided, they will be used with dynamic names
 event_data = {
     "CustomEvent1": [
-        '{"datetime": "2023-01-01 12:00:00", "severity": "Medium", "response": "Investigating", "message": "Custom issue"}',
+        '{"datetime": "2023-01-01 12:00:00", "message": "Custom issue"}',
         '{"datetime": "2023-01-02 15:30:00", "severity": "Low", "response": "No action required", "message": "Another custom message"}'
     ],
     "CustomEvent2": [
