22184a048d7267ea511bda3159c9bed0d3f45e78
nishantmedhi <30440417+nishantmedhi@users.noreply.github.com>
2023-12-05 12:07:38 +0530

    Create template.py

---
+++ custom_components/java_patch/windows/template.py
@@ -0,0 +1,43 @@
+from custom_components.common.logger import Logger
+from custom_components.common.record import EventRecorder
+
+logger = Logger()
+eventRecorder = EventRecorder()
+
+def pre_check_env_windows():
+    eventName = "Validating environment pre-checks in Windows"
+    caller = logger.get_caller()
+    try:
+        print(eventName)
+        if "Windows" in eventName:
+            update_logs(eventRecorder.record(eventName, caller, "SUCCESS"))
+        else:
+            message = "eventName doesn't contain Windows"
+            update_logs(eventRecorder.record(eventName, caller, "FAILURE", "ERROR", message))
+            
+        pre_check_ansible_windows()
+            
+    except Exception as e:
+        update_logs(eventRecorder.record(eventName, caller, "FAILURE", "EXCEPTION", str(e)))
+
+def pre_check_ansible_windows():
+    eventName = "Validating installation of Ansible in Windows"
+    caller = logger.get_caller()
+    try:
+        print(eventName)
+        if "Ansible" in eventName:
+            update_logs(eventRecorder.record(eventName, caller, "SUCCESS"))
+        else:
+            message = "eventName doesn't contain Ansible"
+            update_logs(eventRecorder.record(eventName, caller, "FAILURE", "ERROR", message))
+            
+    except Exception as e:
+        update_logs(eventRecorder.record(eventName, caller, "FAILURE", "EXCEPTION", str(e)))
+       
+def update_logs(eventData):
+    logger_data = Logger(eventData)
+    logger_data.write_to_file()
+    logger.read_and_print_file()
+    
+if __name__ == "__main__":
+    pre_check_env_windows()
